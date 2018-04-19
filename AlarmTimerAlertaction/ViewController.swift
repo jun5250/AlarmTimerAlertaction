f77410fd2b6057782d438d28e9ad84054687a99c
dit <dit@ditui-imac.local>
2018-04-19 16:36:40 +0900

    .

--- AlarmTimerAlertaction/ViewController.swift
+++ AlarmTimerAlertaction/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var myTimer = Timer()
+    @IBOutlet weak var timelbl: UILabel!
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var datelbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        datelbl.text = formatter.string(from: myDatePicker.date)
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime(){
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timelbl.text = formatter.string(from: date)
+        
+        if timelbl.text == datelbl.text {
+            let myAlret = UIAlertController(title : "알림", message: "알람 시간이 되었습니다.!", preferredStyle: .actionSheet)
+            
+            let okAction = UIAlertAction(title: "확인", style: .default, handler : {(myaction: UIAlertAction) -> Void in
+                self.view.backgroundColor = UIColor.red
+            })
+            
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler : nil)
+        myAlret.addAction(okAction)
+        myAlret.addAction(cancelAction)
+        present(myAlret, animated: true, completion: nil)
+        }
     }
-
-
+    @IBAction func changeDatePicker(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        datelbl.text = formatter.string(from: myDatePicker.date)
+    }
+    
+    
+    
+    
 }
+
 
